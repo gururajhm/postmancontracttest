d1d29294985bcd9cfb33718c0cb0b8bee8ff7634
gururajhm <guru@oneflow.io>
2019-09-03 18:19:56 +0100

    folder structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const testData = {
+  name: 'John Doe',
+  age: '30',
+  account_balance: '15.5'
+}
+
+app.get('/', (req, res) => res.send(testData))
+
+app.listen(port, () => console.log(`Server listening on port ${port}`))
